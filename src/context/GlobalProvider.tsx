aa1d95118be5f258f38dc6e996854e1d1098dd22
juan <judagosa8520@gmail.com>
2024-09-26 16:08:12 -0500

    Refactor: Update GlobalProvider and main.tsx for context management

--- src/context/GlobalProvider.tsx
+++ src/context/GlobalProvider.tsx
@@ -2,7 +2,7 @@ import { ReactNode, useMemo, useState } from "react"
 import { ResultCharactersApi } from "../pages/interfaces/characterInterfaceApi";
 import { GlobalContext } from "./GlobalContext";
 
-export const GlobalProvider = ({children}: {children: ReactNode}) => {
+export const GlobalContextProvider = ({children}: {children: ReactNode}) => {
   const [ characters, setCharacters ] = useState<ResultCharactersApi[]>([]);
 
   const contextValue = useMemo(
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';  
 import { BrowserRouter } from 'react-router-dom';
+import { GlobalContextProvider } from './context/GlobalProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <GlobalContextProvider>
+        <App />
+      </GlobalContextProvider>
     </BrowserRouter>
   </StrictMode>
 );
