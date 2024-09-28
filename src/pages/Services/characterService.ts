d85836712bc0b61f35a8dffa9a152914da6cc3d0
juan <judagosa8520@gmail.com>
2024-09-28 14:37:48 -0500

    Refactor: Update characterService to fetch characters with optional parameters

--- src/pages/Services/characterService.ts
+++ src/pages/Services/characterService.ts
@@ -1,9 +1,44 @@
-import axios from "axios";
-import { CharactersAPI } from "../interfaces/characterInterfaceApi";
+import axios from 'axios';
+import { CharactersAPI } from '../interfaces/characterInterfaceAPI';
 
-export const fetchCharacters = async () => {
+interface ParamsProps {
+  name?: string;
+  status?: string;
+  species?: string;
+  type?: string;
+  gender?: string;
+}
+export const fetchCharacters = async ({
+  name,
+  status,
+  species,
+  type,
+  gender,
+}: ParamsProps = {}) => {
   try {
-    const response = await axios.get<CharactersAPI>(`${import.meta.env.VITE_BASE_URL}/character`);
+    // Crear un objeto con todos los parámetros
+    const allParams = { name, status, species, type, gender };
+
+    // Filtrar los parámetros que no son undefined, null o cadena vacía
+    const definedParams = Object.fromEntries(
+      Object.entries(allParams).filter(
+        ([_, value]) => value !== undefined && value !== null && value !== ''
+      )
+    );
+
+    // Inicializar las opciones de la solicitud
+    const options: any = {};
+
+    // Si hay parámetros definidos, añadirlos a las opciones
+    if (Object.keys(definedParams).length > 0) {
+      options.params = definedParams;
+    }
+
+    // Realizar la solicitud con o sin parámetros
+    const response = await axios.get<CharactersAPI>(
+      `${import.meta.env.VITE_BASE_URL}/character`,
+      options
+    );
     return response;
   } catch (error) {
     console.error('Error fetching characters:', error);
