4c9f507ba7dab8217c30447d09ed4da15e79e394
juan <judagosa8520@gmail.com>
2024-09-29 14:53:09 -0500

    refactor: Update Characters page to include loading skeleton

--- src/pages/Characters/Characters.tsx
+++ src/pages/Characters/Characters.tsx
@@ -1,8 +1,8 @@
 import { ChangeEvent, useContext, useEffect, useState } from 'react';
 import { GlobalContext } from '../../context/GlobalContext';
-import { fetchCharacters } from '../Services/characterService';
-import { CharacterCard, PaginationRounded, SearchBar } from '../components';
-import { Container, Box } from '@mui/material';
+import { fetchCharacters } from './Services/characterService';
+import { Container, Box, Skeleton } from '@mui/material';
+import { CharacterCard, PaginationRounded, SearchBar } from './components';
 
 interface Query {
   name: string;
@@ -12,18 +12,27 @@ export const Characters = () => {
   const { characters, setCharacters, search } = useContext(GlobalContext);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+  const [ loading, setLoading ] = useState(false);
+  const [ finalSearch, setFinalSearch ] = useState('');
 
   function handlePaginationChange(_: ChangeEvent<unknown>, value: number) {
     setPage(value);
   }
 
-  const getCharacters = async (query: Query) => {
+  const getCharacters = async () => {
     try {
+      setLoading(true);
+      const query: Query = {
+        name: finalSearch,
+        page: page,
+      };
       const response = await fetchCharacters(query);
       setCharacters(response?.data.results || []);
       setTotalPages(response?.data.info.pages || 1);
     } catch (error) {
       console.error('Error fetching characters:', error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -32,16 +41,16 @@ export const Characters = () => {
   }, [search]);
 
   useEffect(() => {
+    getCharacters();
+  } , [finalSearch, page]);
+
+  useEffect(() => {
     const timer = setTimeout(() => {
-      const query = {
-        name: search,
-        page: page,
-      };
-      getCharacters(query);
+      setFinalSearch(search);
     }, 500);
 
     return () => clearTimeout(timer);
-  }, [search, page]);
+  }, [search]);
 
   return (
     <Container>
@@ -53,9 +62,21 @@ export const Characters = () => {
         justifyContent="center"
         mt={4}
       >
-        {characters.map((character) => (
-          <CharacterCard key={character.id} {...character} />
-        ))}
+        {loading ? (
+          Array.from(new Array(8)).map((_, index) => (
+            <Skeleton
+              key={index}
+              variant="rectangular"
+              width={345}
+              height={400}
+              animation="wave"
+            />
+          ))
+        ) : (
+          characters.map((character) => (
+            <CharacterCard key={character.id} {...character} />
+          ))
+        )}
       </Box>
       <PaginationRounded
         count={totalPages}
