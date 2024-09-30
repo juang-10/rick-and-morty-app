22cb1b1046ced237712a63566ffaf3c02849af74
juan <judagosa8520@gmail.com>
2024-09-30 07:44:36 -0500

    feat: Add Episodes and Locations components to Navigation

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { Characters } from './pages';
+import { Characters, Episodes, Locations } from './pages';
 import { RoutesApp } from './models';
 
 export const Navigation = () => {
   return (
     <Routes>
       <Route path={RoutesApp.CHARACTERS} element={<Characters />} />
+      <Route path={RoutesApp.EPISODES} element={<Episodes />} />
+      <Route path={RoutesApp.LOCATIONS} element={<Locations />} />
       <Route
         path="/*"
         element={<Navigate to={RoutesApp.CHARACTERS} replace />}
