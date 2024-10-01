cccce9a3d35a0a3163203347ae0f3bd9148d1462
juan <judagosa8520@gmail.com>
2024-10-01 15:00:00 -0500

    refactor: Update Navbar component in Characters page to highlight active link

--- src/pages/Characters/components/Navbar.tsx
+++ src/pages/Characters/components/Navbar.tsx
@@ -1,18 +1,52 @@
-import { AppBar, Link, Toolbar, Typography } from "@mui/material"
-import { Link as RouterLink } from "react-router-dom"
-import { RoutesApp } from "../../../models"
+import { AppBar, Link, Toolbar, Typography } from '@mui/material';
+import { Link as RouterLink, useLocation } from 'react-router-dom';
+import { RoutesApp } from '../../../models';
 
 export const Navbar = () => {
+  const path = useLocation();
+
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Rick & Morty WiKi
         </Typography>
-        <Link component={RouterLink} to={RoutesApp.CHARACTERS} color="inherit" sx={{ mr: 2 }}>Characters</Link>
-        <Link component={RouterLink} to={RoutesApp.EPISODES} color="inherit" sx={{ mr: 2 }}>Episode</Link>
-        <Link component={RouterLink} to={RoutesApp.LOCATIONS} color="inherit" sx={{ mr: 2 }}>Location</Link>
+        <Link
+          sx={{
+            textDecoration: path.pathname === '/' ? 'underline' : 'none',
+            mr: 2,
+          }}
+          component={RouterLink}
+          to={RoutesApp.CHARACTERS}
+          color="inherit"
+        >
+          Characters
+        </Link>
+        <Link
+          component={RouterLink}
+          to={RoutesApp.EPISODES}
+          color="inherit"
+          sx={{
+            textDecoration:
+              path.pathname === RoutesApp.EPISODES ? 'underline' : 'none',
+            mr: 2,
+          }}
+        >
+          Episode
+        </Link>
+        <Link
+          component={RouterLink}
+          to={RoutesApp.LOCATIONS}
+          color="inherit"
+          sx={{
+            textDecoration:
+              path.pathname === RoutesApp.LOCATIONS ? 'underline' : 'none',
+            mr: 2,
+          }}
+        >
+          Location
+        </Link>
       </Toolbar>
     </AppBar>
-  )
-}
+  );
+};
