bff75bcfb483f2a975d48382107299f696ed365d
juan <judagosa8520@gmail.com>
2025-02-09 14:38:46 -0500

    feat: Add getEpisodesApi function to fetch episode data from API

---
+++ src/pages/Episodes/services/index.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+import { EpisodesAPI } from "../interfaces/episodes.interface";
+
+export const getEpisodesApi = async (id?: string) => {
+  console.log("🚀 ~ getEpisodesApi ~ id:", id)
+  const response = await axios.get<EpisodesAPI>(id ? `https://rickandmortyapi.com/api/episode/${id}` : 'https://rickandmortyapi.com/api/episode');
+  return response?.data;
+  // return response?.data.characters || [];
+}
